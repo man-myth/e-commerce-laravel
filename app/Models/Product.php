2f73f1805cd7cc23b6de7f53427a67370351e6e9
manmyth <manmeets1100@gmail.com>
2024-07-13 19:22:47 +0800

    Add Category, Product, ProductImage, Review, and ReviewImage model

---
+++ app/Models/Product.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = [
+        "id",
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+}
